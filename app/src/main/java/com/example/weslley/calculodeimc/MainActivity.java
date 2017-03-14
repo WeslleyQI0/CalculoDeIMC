94421b1f62f877bb2d12fbe75faff12de60b1789
Weslley <weslley,cvc@hotmail.com>
2017-03-14 14:35:19 -0300

    Implementando o metodo calcularImc

--- app/src/main/java/com/example/weslley/calculodeimc/MainActivity.java
+++ app/src/main/java/com/example/weslley/calculodeimc/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.weslley.calculodeimc;
 
+import android.annotation.TargetApi;
 import android.icu.text.DecimalFormat;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
@@ -14,9 +15,6 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private String imcTotal;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,41 +22,48 @@ public class MainActivity extends AppCompatActivity {
 
         //Instanciar o Botão Calcular
         Button btCalcular = (Button)findViewById(R.id.btCalcular);
-        Button btAlert = (Button)findViewById(R.id.btAlert);
+        Button btToast = (Button)findViewById(R.id.btToast);
         //Adiciona o Onclick no Botão
         btCalcular.setOnClickListener(new OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View v) {
-                //Instanciar o EditText Peso e Altura
-                EditText edPeso = (EditText)findViewById(R.id.edPeso);
-                EditText edAltura = (EditText)findViewById(R.id.edAltura);
-
-                //Cria Variaveis que Guarda os Valores dos EditText
-                double peso = Double.parseDouble(edPeso.getText().toString());
-                double altura = Double.parseDouble(edAltura.getText().toString());
-
-                //Faz o calculo do IMC
-                double imc = peso/(altura*altura);
-                //Pasa para o valor para decimal
-                DecimalFormat df = new DecimalFormat("0.##");
-                //Converte para ima string
-                String imcTotal = df.format(imc);
-                //Seta o valor no Textview
+                calcularImc();
                 TextView tvResultado = (TextView) findViewById(R.id.tvResultado);
-                tvResultado.setText(imcTotal);
-                //Exibindo Valor em um Toast
-                Toast.makeText(MainActivity.this, "Seu IMC é: "+imcTotal, Toast.LENGTH_SHORT).show();
+                tvResultado.setText("OK");
+            }
+        });
+        btToast.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcularImc();
+                Toast.makeText(MainActivity.this, "Seu IMC é: ",Toast.LENGTH_SHORT).show();
             }
         });
 
-        btAlert.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Seu IMC é: "+imcTotal, Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
 
+    @TargetApi(Build.VERSION_CODES.N)
+    public String calcularImc() {
+        //Instanciar o EditText Peso e Altura
+        EditText edPeso = (EditText)findViewById(R.id.edPeso);
+        EditText edAltura = (EditText)findViewById(R.id.edAltura);
+
+        //Cria Variaveis que Guarda os Valores dos EditText
+        double peso = Double.parseDouble(edPeso.getText().toString());
+        double altura = Double.parseDouble(edAltura.getText().toString());
+
+        //Faz o calculo do IMC
+        double imc = peso/(altura*altura);
+        //Pasa para o valor para decimal
+        DecimalFormat df = new DecimalFormat("0.##");
+        //Converte para ima string
+        String imcTotal = df.format(imc);
+        //Seta o valor no Textview
+        //TextView tvResultado = (TextView) findViewById(R.id.tvResultado);
+        //tvResultado.setText(imcTotal);
+        //Exibindo Valor em um Toast
+        //Toast.makeText(MainActivity.this, "Seu IMC é: "+imcTotal, Toast.LENGTH_SHORT).show();
+        return imcTotal;
+    }
 }
